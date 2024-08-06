503d5f6b945519cbc1732bc526f9af07c88e7736
Ivaldo <ivaldotrindade1245@gmail.com>
2024-08-06 18:21:18 -0300

    Modularização - Curso DevAprender

---
+++ Teste modularização/app.py
@@ -0,0 +1,4 @@
+import audio as a
+
+aumentar = a.aumentar_volume(5)
+diminuir = a.diminuir_volume(5)
---
+++ Teste modularização/audio.py
@@ -0,0 +1,5 @@
+def aumentar_volume(volume):
+    print(f'Volume aumentado em {volume}')
+
+def diminuir_volume(volume):
+    print(f'Volume diminuido em {volume}')    
